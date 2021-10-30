c90aa838fa2c24752b846e0a21050b30dec08e2e
Alex Chiu <alexc@neverbland.com>
2021-10-30 17:00:00 +0100

    feat: generate and save playlist locally

---
+++ scripts/generatePlaylist.ts
@@ -0,0 +1,39 @@
+/**
+ * Fetch playlist from audius and create a local copy
+ */
+
+import * as fs from "fs";
+import axios from "axios";
+import { Playlist } from "../types/playlist.interface";
+
+async function generatePlaylist() {
+  const resp = await axios.get(
+    "https://disc-gru01.audius.hashbeam.com/v1/playlists/DOPRl/tracks?app_name=HACKERFM",
+    {
+      headers: {
+        Accept: "application/json",
+      },
+    }
+  );
+
+  const data = JSON.stringify(resp.data.data) as unknown as Playlist;
+
+  fs.writeFile(
+    "./public/playlist.json",
+    JSON.stringify(data),
+    "utf8",
+    (err) => {
+      if (err) {
+        console.error(
+          "An error occured while writing JSON Object to File.",
+          err
+        );
+        return err;
+      }
+
+      console.log("Playlist has been generated");
+    }
+  );
+}
+
+generatePlaylist();
---
+++ types/playlist.interface.ts
@@ -0,0 +1,56 @@
+export interface Artwork {
+  "150x150": string;
+  "480x480": string;
+  "1000x1000": string;
+}
+
+export interface RemixOf {
+  tracks?: any;
+}
+
+export interface CoverPhoto {
+  "640x": string;
+  "2000x": string;
+}
+
+export interface ProfilePicture {
+  "150x150": string;
+  "480x480": string;
+  "1000x1000": string;
+}
+
+export interface User {
+  album_count: number;
+  bio: string;
+  cover_photo: CoverPhoto;
+  followee_count: number;
+  follower_count: number;
+  handle: string;
+  id: string;
+  is_verified: boolean;
+  location: string;
+  name: string;
+  playlist_count: number;
+  profile_picture: ProfilePicture;
+  repost_count: number;
+  track_count: number;
+}
+
+export interface Playlist {
+  artwork: Artwork;
+  description: string;
+  genre: string;
+  id: string;
+  mood: string;
+  release_date: string;
+  remix_of: RemixOf;
+  repost_count: number;
+  favorite_count: number;
+  tags: string;
+  title: string;
+  user: User;
+  duration: number;
+  downloadable: boolean;
+  play_count: number;
+  permalink: string;
+}
