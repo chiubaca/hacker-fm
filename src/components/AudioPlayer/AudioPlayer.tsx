71e44737f3a01f70f56cb6cf26aac6ac9a2d068a
Alex Chiu <alexc@neverbland.com>
2021-10-30 21:45:44 +0100

    refactor: auto play next track and code cleanup

--- src/components/AudioPlayer/AudioPlayer.tsx
+++ src/components/AudioPlayer/AudioPlayer.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect, useRef } from "react";
 import AudioPlayerManager, { Song } from "./AudioPlayerManager";
 import playlist from "../../../public/playlist.json";
 import { Playlist } from "../../../types/playlist.interface";
+import "./styles.css";
 
 const parsedData = JSON.parse(playlist) as unknown as Playlist;
 
@@ -30,6 +31,32 @@ export function AudioPlayer() {
     AudioPlayerRef.current.playTrack();
   }, []);
 
+  const playNextTrackHandler = () => {
+    if (selectedTrack.index === PLAYLIST.length - 1) {
+      AudioPlayerRef.current.changeTrack(PLAYLIST[0]);
+      setSelectedTrack(PLAYLIST[0]);
+    } else {
+      //decrement track
+      let incrementTrack = PLAYLIST[selectedTrack.index + 1];
+      AudioPlayerRef.current.changeTrack(incrementTrack);
+      setSelectedTrack(incrementTrack);
+    }
+  };
+
+  const playPreviousTrackHandler = () => {
+    if (selectedTrack.index === 0) {
+      // loop to back of the list
+      let decrementTrack = PLAYLIST[PLAYLIST.length - 1];
+      AudioPlayerRef.current.changeTrack(decrementTrack);
+      setSelectedTrack(decrementTrack);
+    } else {
+      //decrement track
+      let decrementTrack = PLAYLIST[selectedTrack.index - 1];
+      AudioPlayerRef.current.changeTrack(decrementTrack);
+      setSelectedTrack(decrementTrack);
+    }
+  };
+
   return (
     <div>
       <audio
@@ -37,58 +64,35 @@ export function AudioPlayer() {
         controls
         ref={audioElement}
         src={PLAYLIST[0].url}
+        onEnded={playNextTrackHandler}
       ></audio>
 
-      <button
-        onClick={() => {
-          if (selectedTrack.index === 0) {
-            // loop to back of the list
-            let decrementTrack = PLAYLIST[PLAYLIST.length - 1];
-            AudioPlayerRef.current.changeTrack(decrementTrack);
-            setSelectedTrack(decrementTrack);
-          } else {
-            //decrement track
-            let decrementTrack = PLAYLIST[selectedTrack.index - 1];
-            AudioPlayerRef.current.changeTrack(decrementTrack);
-            setSelectedTrack(decrementTrack);
-          }
-        }}
-      >
-        ⏮️
-      </button>
-
-      <button onClick={() => AudioPlayerRef.current.playTrack()}>⏯</button>
-
-      <button
-        onClick={() => {
-          if (selectedTrack.index === PLAYLIST.length - 1) {
-            AudioPlayerRef.current.changeTrack(PLAYLIST[0]);
-            setSelectedTrack(PLAYLIST[0]);
-          } else {
-            //decrement track
-            let incrementTrack = PLAYLIST[selectedTrack.index + 1];
-            AudioPlayerRef.current.changeTrack(incrementTrack);
-            setSelectedTrack(incrementTrack);
-          }
-        }}
-      >
-        ⏭️
-      </button>
-
       <div>Selected track is: {selectedTrack.name}</div>
-      <canvas ref={canvasContainer} className="webgl"></canvas>
-      <div className="playerElem">
-        {PLAYLIST.map((track, index) => (
-          <button
-            key={index}
-            onClick={(e) => {
-              setSelectedTrack(track);
-              AudioPlayerRef.current.changeTrack(track);
-            }}
-          >
-            {track.name}
-          </button>
-        ))}
+
+      <div className="audio-player">
+        <canvas ref={canvasContainer} className="orb"></canvas>
+
+        <div className="tracks">
+          {PLAYLIST.map((track, index) => (
+            <button
+              key={index}
+              onClick={(e) => {
+                setSelectedTrack(track);
+                AudioPlayerRef.current.changeTrack(track);
+              }}
+            >
+              {track.name}
+            </button>
+          ))}
+        </div>
+
+        <div className="player-controls">
+          <button onClick={playPreviousTrackHandler}>⏮️</button>
+
+          <button onClick={() => AudioPlayerRef.current.playTrack()}>⏯</button>
+
+          <button onClick={playNextTrackHandler}>⏭️</button>
+        </div>
       </div>
     </div>
   );
