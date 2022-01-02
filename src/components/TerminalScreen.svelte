fa62330f1f2be58d782c97a48e0a23752f0ec89d
Alex Chiu <alexc@neverbland.com>
2022-01-02 20:17:35 +0000

    fix: fine tune terminal ascii art and typing effect

--- src/components/TerminalScreen.svelte
+++ src/components/TerminalScreen.svelte
@@ -11,35 +11,42 @@
 	</Typewriter>
 
 	<Typewriter delay={1000} interval={1} cursor="green">
-		<pre>::::::::::::::::::::::::::::::::::
-      
-    
-      ██   ██  █████   ██████ ██   ██ ███████ ██████     ███████ ███    ███
-      ██   ██ ██   ██ ██      ██  ██  ██      ██   ██    ██      ████  ████
-      ███████ ███████ ██      █████   █████   ██████     █████   ██ ████ ██
-      ██   ██ ██   ██ ██      ██  ██  ██      ██   ██    ██      ██  ██  ██
-      ██   ██ ██   ██  ██████ ██   ██ ███████ ██   ██ ██ ██      ██      ██
+		<pre class="clamp-text ascii-art">
 
+ＷＥＬＣＯＭＥ ＴＯ
 
-::::::::::::::::::::::::::::::::::</pre>
+888    888        d8888  .d8888b.  888    d8P  8888888888 8888888b.      8888888888 888b     d888 
+888    888       d88888 d88P  Y88b 888   d8P   888        888   Y88b     888        8888b   d8888 
+888    888      d88P888 888    888 888  d8P    888        888    888     888        88888b.d88888 
+8888888888     d88P 888 888        888d88K     8888888    888   d88P     8888888    888Y88888P888 
+888    888    d88P  888 888        8888888b    888        8888888P"      888        888 Y888P 888 
+888    888   d88P   888 888    888 888  Y88b   888        888 T88b       888        888  Y8P  888 
+888    888  d8888888888 Y88b  d88P 888   Y88b  888        888  T88b  d8b 888        888   "   888 
+888    888 d88P     888  "Y8888P"  888    Y88b 8888888888 888   T88b Y8P 888        888       888
+	</pre>
 	</Typewriter>
 
-	<Typewriter delay={4000} interval={10} cursor="green">
+	<Typewriter delay={7000} interval={50} cursor="green">
 		<p>Press any key to continue...</p>
 	</Typewriter>
 </div>
 
 <style scoped>
 	.terminal {
+		font-family: monospace;
 		color: green;
 		background-color: #a4508b;
 		background-image: linear-gradient(326deg, #a4508b 0%, #5f0a87 74%);
 		color: white;
-		white-space: pre-wrap;
 		height: 100vh;
-		font-size: clamp(0.4rem, 1vw, 1rem);
 	}
 
+	.ascii-art {
+		padding: 5rem 0;
+	}
+	.clamp-text {
+		font-size: clamp(0.5rem, 1vw, 1rem);
+	}
 	pre {
 		margin: 0;
 	}
