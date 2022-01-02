8b271c17c8ff3d099bcfd25882b43b358d26ebeb
Alex Chiu <alexc@neverbland.com>
2022-01-02 13:52:24 +0000

    config: build static site

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import svg from '@poppanator/sveltekit-svg';
 import preprocess from 'svelte-preprocess';
 import { resolve } from 'path';
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
